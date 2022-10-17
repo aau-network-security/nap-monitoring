90357a812a2401e92c4a012256740d560270d0e1
Rasmi V. Mahmoud <rvm@es.aau.dk>
2022-10-17 20:58:40 +0200

    automate fleet agent signup

--- ubuntu-20.04SOC/scripts/install_tools.sh
+++ ubuntu-20.04SOC/scripts/install_tools.sh
@@ -21,6 +21,8 @@ apt-get install ifupdown -y
 apt-get install zip  -y
 apt-get install unzip -y
 
+sudo apt-get install jq
+sudo apt-get install vim
 ## install netman service to manage down network interfaces
 # pop up version if required
 mkdir /home/vagrant/netman && cd /home/vagrant/netman
@@ -70,19 +72,30 @@ curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
 
-#cd /home/vagrant/
-#curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.13.4-linux-x86_64.tar.gz
-#tar xzvf elastic-agent-7.13.4-linux-x86_64.tar.gz
-#cd elastic-agent-7.13.4-linux-x86_64
+cd /home/vagrant/
+curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.6-linux-x86_64.tar.gz
+tar xzvf elastic-agent-7.17.6-linux-x86_64.tar.gz
+cd elastic-agent-7.17.6-linux-x86_64
 
+#Gerate Fleet Service Account
+$token=$(curl -k -u "elastic:changeme" -s -X POST localhost:9200/_security/service/elastic/fleet-server/credential/token --header 'kbn-xsrf: true'| jq -r .token.value)
+
+sudo ./elastic-agent install --fleet-server-es=http://localhost:9200 --fleet-server-service-token=$token --fleet-server-policy=499b5aa7-d214-5b5d-838b-3cd76469844e
 ####install fleet server
 #sudo ./elastic-agent install -f --url=http://localhost:8220 --fleet-server-service-token=AAEAAWVsYXN0aWMvZmxlZXQtc2VydmVyL3Rva2VuLTE2MzAzMTQ4NjYzODg6WUk4QVN2anBUR095ZkdRVVRDLVVTZw --insecure
 
 #sudo ./elastic-agent install -f --fleet-server-es=http://localhost:9200 --fleet-server-service-token=AAEAAWVsYXN0aWMvZmxlZXQtc2VydmVyL3Rva2VuLTE2MzAzMTQ4NjYzODg6WUk4QVN2anBUR095ZkdRVVRDLVVTZw
 
 
+
+
+
 systemctl daemon-reload
-#systemctl enable elastic-agent
+systemctl enable elastic-agent
+
+
+
+
 
 
 ####start fleet server
