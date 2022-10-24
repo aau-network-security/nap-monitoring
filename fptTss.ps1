c560624461a179e75ee1eb5a01a59b8df3cdfabe
Rasmi V. Mahmoud <rvm@es.aau.dk>
2022-10-24 15:04:09 +0200

    final version ftp Script

--- fptTss.ps1
+++ fptTss.ps1
@@ -1,40 +1,61 @@
-function Receive-FTP([string]$user, [string]$pass, [string]$site, [string]$key, [string]$source, [string]$dest){
-    try
-    {
 
-        # Setup session options
-        $sessionOptions = New-Object WinSCP.SessionOptions
-        $sessionOptions.Protocol = [WinSCP.Protocol]::Sftp
-        $sessionOptions.HostName = "$site"
-        $sessionOptions.UserName = "$user"
-        $sessionOptions.Password = "$pass"
-        $sessionOptions.SshHostKeyFingerprint = "$key"
 
-        $session = New-Object WinSCP.Session
+Write-Host "Sleeping for 30s..."
+Start-Sleep 30
 
+$remote = "/upload/*";
+$sync = "C:\Users\John Doe\Downloads\sync\*";
+$wildcard = "*.zip"
+
+set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+
+try
+{
+# Load WinSCP .NET assembly
+Add-Type -Path "${env:ProgramFiles(x86)}\WinSCP\WinSCPnet.dll"
+
+# Set up session options
+$sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+    Protocol = [WinSCP.Protocol]::Sftp
+    HostName = xxxxxx
+    PortNumber = xxx
+    UserName = xxx
+    Password = xxx
+    SshHostKeyFingerprint = xxxxx
+}
+
+$session = New-Object WinSCP.Session
         try
         {
             # Connect
             $session.Open($sessionOptions)
 
             #List Dir
-            $listDir = $session.ListDirectory($ftpSource)
+            $listFiles = $session.EnumerateRemoteFiles($remote, $wildcard, [EnumerationOptions]::None)
 
-            foreach ($file in $listDir.Files.) {
+            foreach ($file in $listFiles) {
 
-                $transferResult = $session.GetFiles($ftpSource, $ftpDest).Check()
+                echo "$file"
+                $transferResult = $session.GetFilesToDirectory($remote, $sync, "true")
+                break
+
             }
 
             # Print results
-            foreach ($transfer in $transferResult.Transfers)
+            $sync = get-childitem -Filter * "C:\Users\John Doe\Downloads\sync\*"
+            foreach ($transfer in $sync)
                 {
                     Write-Host  -foregroundcolor green ("Upload of {0} succeeded" -f $transfer.FileName)
-                            if ([System.IO.Path]::GetExtension("$transfer") -ieq ".zip")
-                            {
-                                sz x "$transfer" -o"C:\Users\John Doe\Downloads\runbaby\" -p "infected"
-                                Remove-Item $transfer.Fullname
-                            }
-                    break
+                    if ([System.IO.Path]::GetExtension($transfer.Fullname) -ieq ".zip")
+                    {
+                        sz x $transfer.Fullname -o"C:\Users\John Doe\Downloads\runbaby\" -p "infected"
+                        Remove-Item $transfer.Fullname
+                    }
+                    else{
+                      echo "Problem with the archive"
+
+                    }
+
                 }
         }
             finally
@@ -59,7 +80,7 @@ function Receive-FTP([string]$user, [string]$pass, [string]$site, [string]$key, 
            Stop-Computer -ComputerName localhost
         }
       else{
-          Rename-Item $danger.Fullname -newName "$file.exe"
+          Rename-Item $danger.Fullname -newName "$danger.exe"
           Start-Process $danger.Fullname -ArgumentList "/s"
           Start-Sleep -Seconds 600
           Stop-Computer -ComputerName localhost
